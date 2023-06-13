8a164a9e0f3b951472d73d2cd94c08a4093fedf4
Sara Khan Tareen <sara.tareen@gmail.com>
2023-06-13 18:51:05 +0500

    - Creating router links for the navbar items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TransactionHistoryComponent } from './transaction-history/transaction-history.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoute: Routes = [
+  {path: 'Home', component: NavbarComponent},
+  {path: 'History', component: TransactionHistoryComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    TransactionHistoryComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
